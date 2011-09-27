9ccdcd400a8aa10099e1d401394f5a411db0cdb8
Joao Silva <kappy@acydburne.com.pt>
2011-09-27 17:25:27 +0100

    Added nuspec and support for exporting to nuget

--- OSDBnet/Properties/AssemblyInfo.cs
+++ OSDBnet/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OSDBnet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An open source library to consume the OpenSubtitles.org service written in C#")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Kappy")]
 [assembly: AssemblyProduct("OSDBnet")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: CLSCompliant(true)]
